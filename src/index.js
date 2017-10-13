b452b4b68690d9600ad99519a20a330d2c187cd2
Neil Bourgeois <neil.bourgeois@gmail.com>
2017-10-13 07:47:46 -0600

    removed boilerplate react app stuff

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class App extends Component {
+  constructor() {
+    super()
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <br/>
+            <h1>Camputer</h1>
+            <hr/><br/>
+            <h2>Temperature</h2>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
--- tests/index.test.js
+++ tests/index.test.js
@@ -5,9 +5,9 @@ const TEST_URL = process.env.TEST_URL;
 fixture('/').page(`${TEST_URL}/`);
 
 test(`users should be able to view the '/' page`, async (t) => {
-
   await t
     .navigateTo(TEST_URL)
     .expect(Selector('H1').withText('Camputer').exists).ok()
-
+    .expect(Selector('H2').withText('Temperature').exists).ok()
+    
 });
