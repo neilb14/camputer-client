a17369a73dd634a67570fe23f8b90705e9c47e89
Neil Bourgeois <neil.bourgeois@gmail.com>
2017-10-13 07:40:31 -0600

    added testcafe

---
+++ tests/index.test.js
@@ -0,0 +1,13 @@
+import { Selector } from 'testcafe';
+
+const TEST_URL = process.env.TEST_URL;
+
+fixture('/').page(`${TEST_URL}/`);
+
+test(`users should be able to view the '/' page`, async (t) => {
+
+  await t
+    .navigateTo(TEST_URL)
+    .expect(Selector('H1').withText('Camputer').exists).ok()
+
+});
