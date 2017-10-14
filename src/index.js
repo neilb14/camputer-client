c1ba584f8394974f45d955cc92922e4d251b1201
Neil Bourgeois <neil.bourgeois@gmail.com>
2017-10-14 08:07:17 -0600

    added last humidity reading to index

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-        temperature: {}
+        temperature: {},
+        humidity: {}
     }
   }
   
@@ -15,24 +16,39 @@ class App extends Component {
     .then((res) => { this.setState({temperature:res.data}); })
     .catch((err) => { console.log(err); })
   }
+  
+  getLastHumidity() {
+    axios.get(`${process.env.REACT_APP_CAMPUTER_SERVICE_URL}/humidities/last`)
+    .then((res) => { this.setState({humidity:res.data}); })
+    .catch((err) => { console.log(err); })
+  }
 
   componentDidMount(){
       this.getLastTemperature();
+      this.getLastHumidity();
   }
 
   render() {
     return (
       <div className="container">
         <div className="row">
-          <div className="col-md-4">
+          <div className="col-md-6">
             <br/>
             <h1>Camputer</h1>
             <hr/><br/>
-            <h2>Temperature</h2>
-            <h4 key={this.state.temperature.id}><strong>{this.state.temperature.value}</strong>{this.state.temperature.uom}</h4>
           </div>
         </div>
-      </div>
+        <div className="row">
+            <div className="col-md-3">
+                <h2>Temperature</h2>
+                <h4 key={this.state.temperature.id}><strong>{this.state.temperature.value}</strong>{this.state.temperature.uom}</h4>
+            </div>
+            <div className="col-md-3">
+                <h2>Humidity</h2>
+                <h4 key={this.state.humidity.id}><strong>{this.state.humidity.value}</strong>{this.state.humidity.uom}</h4>
+            </div>
+          </div>
+        </div>
     )
   }
 }
