9e9c489d7fadedc773586c8ef2ca9d6bd43b1d8c
Neil Bourgeois <neil.bourgeois@gmail.com>
2017-12-03 14:54:24 -0700

    added background image

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #! /usr/local/bin/fish
-export REACT_APP_CAMPUTER_SERVICE_URL=http://192.168.0.198/api
+export REACT_APP_CAMPUTER_SERVICE_URL=http://192.168.0.122/api
 npm run build
 export REACT_APP_CAMPUTER_SERVICE_URL=http://127.0.0.1:5000
-scp -r build pi@192.168.0.198:camputer-client
+scp -r build neilb@192.168.0.122:camputer-client
